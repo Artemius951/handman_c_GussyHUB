67cc9d2d25ea6225a53a5889dce7366a79173006
LookAsLukas <kolyabetmen231006@gmail.com>
2025-02-22 17:40:11 +0700

    lol

--- game.c
+++ game.c
@@ -11,9 +11,9 @@ int get_random(int max) {
     return rnum;
 }
 int select_category(categories_t *categories) {
-    printf("Choose category (pls)");
+    printf("Choose category (pls)\n");
     for (int i = 0; i < categories->categories_cnt; i++) {
-        printf("%d %s", i, categories->category_words[i].category);
+        printf("%d %s\n", i, categories->category_words[i].category);
     }
     int x = 0;
     scanf("%d", &x);
@@ -23,21 +23,22 @@ int select_category(categories_t *categories) {
 int select_level() {
     int x = 999;
     do {
-        printf("Now choose lvl");
-        printf("0. Easy");
-        printf("1. Medium");
-        printf("2. Hard (rock)");
+        printf("Now choose lvl\n");
+        printf("0. Easy\n");
+        printf("1. Medium\n");
+        printf("2. Hard (rock)\n");
         scanf("%d",&x);
+        
     }
     while ((x != 0) && (x != 1) && (x != 2));
     if (x == 0) {
-        printf("You chose easy level. You're sweety UwU");
+        printf("You chose easy level. You're sweety UwU\n");
     }
     else if (x == 1) {
-        printf("You chose medium level. It must be fun");
+        printf("You chose medium level. It must be fun\n");
     }
     else if (x == 2) {
-        printf("You chose hard level. Let the fire burn");
+        printf("You chose hard level. Let the fire burn\n");
     }
     return x;
 }
@@ -72,21 +73,29 @@ int alphabetidx(char *alphabet, char x) {
     return -1;
 }
 
+int is_guess_repeated(game_t *game, char guess) {
+    return game->chosen_alphas[alphabetidx("abcdefghijklmnopqrstuvwxyz", guess)] != '_';
+}
+
 void check_guess(game_t *game, char guess) {
     static char alphabet[ALPHABET_SIZE] = "abcdefghijklmnopqrstuvwxyz";
 
     char *nword = game->word;
-    for (size_t i = 0; i < strlen(nword);i++) {
-        if (guess == nword[i]) {
+    int is_in = 0;
+
+    for (int i = 0; nword[i] != '\0'; i++) {
+        if (nword[i] == guess) {
             game->guessed_word[i] = guess;
             int symb = alphabetidx(alphabet, nword[i]);
             game->chosen_alphas[symb] = 'V';
+            is_in = 1;
         }
-        else {
-            int symb = alphabetidx(alphabet, nword[i]);
-            game->chosen_alphas[symb] = 'X';
-            game->hangman_state++;
-        }
+    }
+
+    if (!is_in) {
+        int symb = alphabetidx(alphabet, guess);
+        game->chosen_alphas[symb] = 'X';
+        game->hangman_state++;
     }
 }
 
@@ -99,18 +108,42 @@ int is_game_won(game_t *game) {
     return 1;
 }
 
+int letter_in_word(char letter, char *word) {
+    for (int i = 0; word[i] != '\0'; i++) {
+        if (word[i] == letter) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void play_game(game_t *game) {
     while (game->hangman_state != MAX_TRIES) {
         print_progress_bar(game->tries_left);
         print_game_state(game);
-        printf("Choose your letter");
+        printf("Choose your letter: ");
         char x;
+        getchar();
         scanf("%c", &x);
+        if (is_guess_repeated(game, x)) {
+            printf("You have already guessed this letter, try a different one)\n");
+            continue;
+        }
         check_guess(game,x);
+        if (!letter_in_word(x, game->word)) {
+            game->tries_left--;
+        }
         if (is_game_won(game)) {
+            print_progress_bar(game->tries_left);
+            print_game_state(game);
             printf("YOU WOOOON!!! URA!");
             return;
         }
     } 
+    for (int i = 0; game->word[i] != '\0'; i++) {
+        game->guessed_word[i] = game->word[i];
+    }
+    print_progress_bar(game->tries_left);
+    print_game_state(game);
     printf("You lost. His death is your fault");
 }
